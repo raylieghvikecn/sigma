06759cd08467378617533849c26b67197b9b38be
sigma <zhaopengwei@csii.com.cn>
2017-11-15 09:46:32 +0800

    数据与数据监听的修改

--- node/index.js
+++ node/index.js
@@ -1,4 +1,5 @@
 var wilddog = require('wilddog');
+var initjson = require('./init.json');
 
 var config = {
 	syncURL: "https://helloworldsigma33.wilddogio.com" //输入节点 URL
@@ -6,9 +7,8 @@ var config = {
 wilddog.initializeApp(config);
 var ref = wilddog.sync().ref();
 
-ref.child("anArray").set([{name: 1, arr: [1,2,3]}, {name: 2, arr: [1,2,3]}, {name: 3, arr: [1,2,3]}]);
-ref.child("anObject").set({name: 'haha', age: '100'});
+ref.child("list").set(initjson);
 
-ref.on("value", function(snapshot) {
+ref.child("list/1").on("value", function(snapshot) {
     console.log(snapshot.val());
 });
