08bc98845cef459d7a02204cb32d23d92983d400
sigma <zhaopengwei@csii.com.cn>
2017-12-12 16:36:18 +0800

    许久

--- node/utils/obj2md5.js
+++
@@ -1,9 +0,0 @@
-var str2md5 = require('./str2md5.js');
-
-module.exports = function(obj) {
-	var temp = "";
-	for (var key in obj) {
-		temp += obj[key] + "";
-	}
-	return str2md5(temp);
-}
--- node/utils/str2md5.js
+++
@@ -1,8 +0,0 @@
-// 将Object和Array转为md5
-
-var crypto = require('crypto');
-
-module.exports = function(str) {
-    var md5 = crypto.createHash('md5');
-    return md5.update(str).digest('hex');
-}
